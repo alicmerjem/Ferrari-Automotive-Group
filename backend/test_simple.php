16461055e0868e28a306e2b521f236d98aade557
alicmerjem <merjem.alic@stu.ibu.edu.ba>
2025-11-01 14:16:02 +0100

    .

--- backend/test_simple.php
+++ backend/test_simple.php
@@ -6,8 +6,6 @@ require_once __DIR__ . '/dao/ContactDao.php';
 require_once __DIR__ . '/dao/ServiceDao.php';
 require_once __DIR__ . '/dao/TestDriveDao.php';
 
-echo "=== Simple DAO Test (Like Classmates) ===\n\n";
-
 $userDao = new UserDao();
 $carDao = new CarDao();
 $staffDao = new StaffDao();
