0ca23bdcdd22c0c20ca350f18d7f93bc745cfa82
alicmerjem <merjem.alic@stu.ibu.edu.ba>
2025-11-15 19:09:54 +0100

    Swagger

---
+++ backend/public/v1/docs/doc_setup.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @OA\OpenApi(openapi="3.0.0")
+ */
+/**
+* @OA\Info(
+*     title="Ferrari Automotive Group API",
+*     description="API for managing Ferrari cars, users, staff, contacts, services, and test drives",
+*     version="1.0.0",
+*     @OA\Contact(
+*         email="your-email@example.com",
+*         name="Your Name"
+*     )
+* )
+*/
+/**
+* @OA\Server(
+*     url= "http://localhost/MerjemAlic/Ferrari-Automotive-Group/backend",
+*     description="API server"
+* )
+*/
+/**
+* @OA\SecurityScheme(
+*     securityScheme="ApiKey",
+*     type="apiKey",
+*     in="header",
+*     name="Authentication"
+* )
+*/
+?>
