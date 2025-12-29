1244f9ae6d89e0f5b6e4aa6e6724e3c17dca7721
alicmerjem <merjem.alic@stu.ibu.edu.ba>
2025-12-29 01:51:23 +0100

    fix

--- backend/index.php
+++ backend/index.php
@@ -1,5 +1,6 @@
 <?php
 
+header("Access-Control-Allow-Origin: https://hammerhead-app-jwr47.ondigitalocean.app");
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization, Authentication');
