38f0a9ad0ac5068fd31ecadb97afb0ff88ea7be4
alicmerjem <merjem.alic@stu.ibu.edu.ba>
2025-12-29 02:21:50 +0100

    fix again

--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -1,5 +1,5 @@
 let Constants = {
-    PROJECT_BASE_URL:"https://hammerhead-app-jwr47.ondigitalocean.app/backend/",
+    PROJECT_BASE_URL:"https://hammerhead-app-jwr47.ondigitalocean.app/",
     USER_ROLE: "user",
     ADMIN_ROLE: "admin"
 }
